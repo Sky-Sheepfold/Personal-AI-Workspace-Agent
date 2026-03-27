07d0af4d8fb3b8330050d750e4cce594c03c71f7
Sheepfold <2786735826@qq.com>
2026-03-27 11:37:29 +0800

    初始化提交

---
+++ src/test/java/com/sheepfold/personalaiworkspaceagent/PersonalAiWorkspaceAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheepfold.personalaiworkspaceagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAiWorkspaceAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
